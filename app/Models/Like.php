15b29869d80aa8579f8d692d425fb947d5e3af87
Ravindra-unuri <ravindraunuri@gmail.com>
2024-03-11 15:11:16 +0530

    Created Like & Comment migration

---
+++ app/Models/Like.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasFactory;
+    protected $table = 'like';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'blogpost_id'
+    ];
+}
