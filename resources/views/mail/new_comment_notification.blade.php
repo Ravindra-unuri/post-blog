e2785f67a57fed8690c76f8da57cceb95637c6ce
Ravindra-unuri <ravindraunuri@gmail.com>
2024-03-20 15:30:13 +0530

    Created mailnotification for NewComment

---
+++ resources/views/mail/new_comment_notification.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Comment Notification</title>
+</head>
+<body>
+    <h1>New Comment Notification</h1>
+    <p>{{ $commenterName }} commented on your blog post.</p>
+    <p>Comment: {{ $commentDetail }}</p>
+</body>
+</html>
