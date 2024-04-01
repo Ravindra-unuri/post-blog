dd53228f47dabddb1463b689f2cf0a91beca2b04
Ravindra-unuri <ravindraunuri@gmail.com>
2024-04-01 19:23:35 +0530

    Implimented CommentRequest

--- app/Http/Requests/CommentRequest.php
+++ app/Http/Requests/CommentRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BlogpostControllerRequest extends FormRequest
+class CommentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,10 +24,7 @@ class BlogpostControllerRequest extends FormRequest
     public function rules()
     {
         return [
-            'blogpost_name' =>'required',
-            'category_id' =>'required',
-            'blogpost_desc' =>'required|min:7|max:100',
-            'upload_file' =>'required',
+            'comment' => 'required'
         ];
     }
 }
